cac67609f40519a6902b9ebe439e57755fdf52e6
Peter <p_vdlinde@hotmail.com>
2021-09-07 17:39:26 +0200

    geupdate versie bookchoice

--- bookchoice.py
+++ bookchoice.py
@@ -9,14 +9,25 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.bookchoice.com/")
 
 
-time.sleep(20)
-
 driver.implicitly_wait(2)
 driver.find_element_by_xpath('//button[normalize-space()="Accepteer cookies"]').click()
 
-inloggen = driver.find_element_by_id('login-link')
+driver.implicitly_wait(10)
+
+inloggen = driver.find_element_by_link_text('Inloggen')
+inloggen.click()
 driver.implicitly_wait(3)
+
 email = driver.find_element_by_id('login-email')
+driver.implicitly_wait(2)
+email.send_keys('p_vdlinde@hotmail.com')
+driver.implicitly_wait(1)
+password = driver.find_element_by_id('login-password')
+password.send_keys('pipo1980_')
+
+
 
 
 time.sleep(60)
+
+#nu wel iets toch?
